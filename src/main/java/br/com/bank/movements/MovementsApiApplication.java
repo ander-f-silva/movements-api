3b6962e70008d1f9162dde69b5facefe03e40787
andferreira <anderson.ferreira@mercadolivre.com>
2021-10-25 19:13:20 -0300

    build: Create the struct of the project
    Add the dependencies of the project using the framework Spring Boot

---
+++ src/main/java/br/com/bank/movements/MovementsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bank.movements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovementsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovementsApiApplication.class, args);
+	}
+
+}

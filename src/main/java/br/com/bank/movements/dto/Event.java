de62cfbd24bcaeeaba6c8bcef4e3c8f4fc75d1ce
andferreira <anderson.ferreira@mercadolivre.com>
2021-10-26 17:46:16 -0300

    feat: Add the annotation to use the constructon with arguments
    For create a new object fake I add a un new annotation

--- src/main/java/br/com/bank/movements/dto/Event.java
+++ src/main/java/br/com/bank/movements/dto/Event.java
@@ -2,6 +2,7 @@ package br.com.bank.movements.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Event {
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     @JsonProperty("type")

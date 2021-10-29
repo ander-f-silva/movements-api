4c1978da4cd76d3deeca436427c6543767a11031
andferreira <anderson.ferreira@mercadolivre.com>
2021-10-29 12:54:40 -0300

    fix: Fix to calculate the balance to account origin
    Fix in time of development when maked the feature

--- src/main/java/br/com/bank/movements/business/TransferMovement.java
+++ src/main/java/br/com/bank/movements/business/TransferMovement.java
@@ -16,15 +16,15 @@ class TransferMovement implements MovementOperation {
 
     @Override
     public Optional<EventResult> effect(final Event event) {
-        var transferEventToAccountDestination = Event.builder()
+        var transferEventToAccountOrigin = Event.builder()
                 .origin(event.getOrigin())
                 .amount(event.getAmount().negate())
                 .build();
 
-        eventRepository.register(event.getDestination(), transferEventToAccountDestination);
-        eventRepository.register(event.getOrigin(), event);
+        eventRepository.register(event.getOrigin(), transferEventToAccountOrigin);
+        eventRepository.register(event.getDestination(), event);
 
-        var eventsOfTheAccountOrigin = eventRepository.listEventsByAccount(event.getOrigin());
+        var eventsOfTheAccountOrigin = eventRepository.listEventsByAccount(transferEventToAccountOrigin.getOrigin());
         var eventsOfTheAccountDestination = eventRepository.listEventsByAccount(event.getDestination());
 
         return Optional.of(new EventResult(new Account(event.getOrigin(), Balance.calculate(eventsOfTheAccountOrigin)), new Account(event.getDestination(), Balance.calculate(eventsOfTheAccountDestination))));
